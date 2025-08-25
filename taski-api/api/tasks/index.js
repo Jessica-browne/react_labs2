4d1390d9bbfc97c066893a04303053c219c5abb2
Jessica Browne <jessicabrowne031@gmail.com>
2025-08-25 19:56:33 +0100

    Mongo lab Error Handling

--- taski-api/api/tasks/index.js
+++ taski-api/api/tasks/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Task from './taskModel';
-
+import asyncHandler from 'express-async-handler';
 const router = express.Router(); // eslint-disable-line
 
 // Get all tasks
@@ -9,11 +9,10 @@ router.get('/', async (req, res) => {
     res.status(200).json(tasks);
 });
 
-// create a task
-router.post('/', async (req, res) => {
+router.post('/', asyncHandler(async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
-});
+}));
 
 // Update Task
 router.put('/:id', async (req, res) => {
@@ -40,5 +39,6 @@ router.delete('/:id', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
 });
+
 
 export default router;
